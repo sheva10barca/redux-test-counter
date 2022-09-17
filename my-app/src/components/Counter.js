211b3784eb317ae7fdc393559fc07d25379aff43
sheva <vladislavshevchenko10@gmail.com>
2022-09-17 15:56:49 +0300

    comment

---
+++ my-app/src/components/Counter.js
@@ -0,0 +1,27 @@
+import { connect } from "react-redux";
+import {inc, dec, rnd} from '../actions';
+import { useSelector, useDispatch } from "react-redux";
+
+const Counter = () => {
+
+   const counter = useSelector(state => state.counter)
+   const dispatch = useDispatch();
+
+   return (
+      <div className="jumbotron">
+         <h1>{counter}</h1>
+         <button onClick={() => dispatch(dec())} className="btn btn-primary">DEC</button>
+         <button onClick={() => dispatch(inc())} className="btn btn-primary">INC</button>
+         <button onClick={() => dispatch(rnd())} className="btn btn-primary">RND</button>
+      </div>
+   )
+}
+
+// const mapStateToProps = (state) => {
+//    return {
+//       counter: state.value
+//    }
+// }
+
+// export default connect(mapStateToProps, actions)(Counter);
+export default Counter;
---
+++ my-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {legacy_createStore as createStore} from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
+
+const store = createStore(reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <Provider store={store}>
+        <App/>
+      </Provider>
+  </React.StrictMode>
+);
